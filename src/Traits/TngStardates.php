596029a9ee27ebef2c94fcbbe5ce404daaab92f1
Nick Moline <nick.moline@jobscan.co>
2023-07-23 23:41:43 -0700

    should be static if called statically

--- src/Traits/TngStardates.php
+++ src/Traits/TngStardates.php
@@ -7,9 +7,9 @@ use Illuminate\Support\Carbon;
 
 trait TngStardates
 {
-    protected $tngZeroDate = '2318-07-05 12:00:00'; // July 5, 2318 at roughly noon Starfleet Command Time is Stardate 00000.0
-    protected $starfleetCommandZone = 'America/Los_Angeles'; // Starfleet Command is in San Francisco
-    protected $millisecondsPerTngStardate = 34367056.4; // 1 Stardate = 34367056.4 milliseconds
+    protected static $tngZeroDate = '2318-07-05 12:00:00'; // July 5, 2318 at roughly noon Starfleet Command Time is Stardate 00000.0
+    protected static $starfleetCommandZone = 'America/Los_Angeles'; // Starfleet Command is in San Francisco
+    protected static $millisecondsPerTngStardate = 34367056.4; // 1 Stardate = 34367056.4 milliseconds
 
     public static function tngZeroDate() : string
     {
